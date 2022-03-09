0eac50cf4d5076963ddeec8b4ce6cb5835e20078
liusen <yjghdsss@163.com>
2022-03-09 16:02:42 +0800

    fix some build env

--- config-and-script/miner-server/build.sh
+++ config-and-script/miner-server/build.sh
@@ -1,5 +1,10 @@
 - compile lotus
 ```
 ## use CUDA
-RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 FFI_USE_CUDA=1 make all
+RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 FFI_USE_CUDA=1 make all lotus-bench
+```
+
+- install dependencies 
+```
+sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
 ```
